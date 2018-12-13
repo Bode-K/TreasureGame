62dc4027d35dbeee128000e943858b341e3bb643
Connor <crichardson914@lsr7.net>
2018-12-13 13:27:56 -0600

    level 3 finished

--- Game.py
+++ Game.py
@@ -20,6 +20,7 @@ W = [255, 255, 255] # white
 
 score = 0
 
+'''
 for turns in range(5):
 
   coinx = randint(0,7)
@@ -74,6 +75,8 @@ for turns in range(5):
     sense.set_pixel(x, y, W)
 
 sense.show_message("Score: " + str(score))
+'''
+'''
 if score == 5:
   sense.show_message("Level 2")
   def wait_for_move():
@@ -87,8 +90,6 @@ if score == 5:
   G = [0, 255, 0] # green
   W = [255, 255, 255] # white
 
-  score = 0
-
   for turns in range(5):
 
     coinx = randint(0,7)
@@ -96,7 +97,7 @@ if score == 5:
     print(coinx, coiny)
 
     sense.set_pixel(coinx, coiny, Y)
-    sleep(2)
+    sleep(1.5)
     sense.clear(Y)
     sleep(1)
     sense.clear()
@@ -143,80 +144,89 @@ if score == 5:
 
       sense.set_pixel(x, y, W)
   sense.show_message("Score: " + str(score))
-  if score == 5:
-    sense.show_message("Level 3")
-    def wait_for_move():
-        while True:
-           e = sense.stick.wait_for_event()
-           if e.action != ACTION_RELEASED:
-             return e
-
-        R = [255, 0, 0]  # red
-        Y = [255, 255, 0] # yellow
-        G = [0, 255, 0] # green
-        W = [255, 255, 255] # white
-
-        score = 0
-
-        for turns in range(5):
-
-          coinx = randint(0,7)
-          coiny = randint(0,7)
-          print(coinx, coiny)
-
-          sense.set_pixel(coinx, coiny, Y)
-          sleep(1)
-          sense.clear(Y)
-          sleep(1)
-          sense.clear(Y)
-          sleep(1)
-          sense.clear()
-
-          x = randint(0,7)
-          y = randint(0,7)
-          sense.set_pixel(x, y, W)
-
-          while True:
-          
-            e = wait_for_move()
-            
-            if e.direction == DIRECTION_MIDDLE:
-
-                if x == coinx and y == coiny:
-                    sense.set_pixel(x, y, G)
-                    score += 1
-
-
-
-                else:
-                    sense.set_pixel(x, y, R)
-
-                     
-              
-                sleep(1.25)
-                sense.clear()
-                sleep(0.5)
-
-
-                break;
-
-
-                     
-            sense.clear()
-            if e.direction == DIRECTION_UP and y > 0:
-                y = y - 1
-            elif e.direction == DIRECTION_DOWN and y < 7:
-                y = y + 1
-            elif e.direction == DIRECTION_LEFT and x > 0:
-                x = x - 1
-            elif e.direction == DIRECTION_RIGHT and x < 7:  
-                x = x + 1
-
-            sense.set_pixel(x, y, W)
-            sense.show_message("Score: " + str(score))
-    
-  else: sense.show_message("You Lose")
 else:
   sense.show_message("You Lose")
+'''
+score = 10
+if score == 10:
+  sense.show_message("Level 3")
+  def wait_for_move():
+      while True:
+        e = sense.stick.wait_for_event()
+        if e.action != ACTION_RELEASED:
+          return e
+
+  R = [255, 0, 0]  # red
+  Y = [255, 255, 0] # yellow
+  G = [0, 255, 0] # green
+  W = [255, 255, 255] # white
+
+  for turns in range(5):
+
+    coinx = randint(0,7)
+    coiny = randint(0,7)
+    print(coinx, coiny)
+
+    sense.set_pixel(coinx, coiny, Y)
+    sleep(1)
+    sense.clear(Y)
+    sleep(1)
+    sense.clear()
+    sleep(0.5)
+    sense.clear(G)
+    sleep(1)
+    sense.clear()
+    sleep(0.5)
+    sense.clear(R)
+    sleep(1)
+    sense.clear()
+  
+
+    x = randint(0,7)
+    y = randint(0,7)
+    sense.set_pixel(x, y, W)
+
+    while True:
+    
+      e = wait_for_move()
+      
+      if e.direction == DIRECTION_MIDDLE:
+
+          if x == coinx and y == coiny:
+              sense.set_pixel(x, y, G)
+              score += 1
+
+
+
+          else:
+              sense.set_pixel(x, y, R)
+
+               
+        
+          sleep(1.25)
+          sense.clear()
+          sleep(0.5)
+
+
+          break;
+
+
+               
+      sense.clear()
+      if e.direction == DIRECTION_UP and y > 0:
+          y = y - 1
+      elif e.direction == DIRECTION_DOWN and y < 7:
+          y = y + 1
+      elif e.direction == DIRECTION_LEFT and x > 0:
+          x = x - 1
+      elif e.direction == DIRECTION_RIGHT and x < 7:  
+          x = x + 1
+
+      sense.set_pixel(x, y, W)
+  sense.show_message("Score: " + str(score))
+            
+else:
+  sense.show_message("You Lose")
+
 
  
